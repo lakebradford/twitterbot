eff4dbeb2caa572b92fb134dcc64c638986152ef
lakebradford <89942749+lakebradford@users.noreply.github.com>
2021-09-09 19:03:16 -0400

    Add files via upload

---
+++ twitterbot.py
@@ -0,0 +1,43 @@
+#You will need to PIP INSTALL tweepy for this to work  
+import tweepy
+import time
+
+consumer_key = '' #to get this information you have to have a developer twitter account https://developer.twitter.com/en/apply-for-access
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+user = api.me()
+print (user.name) #prints your name.
+print (user.screen_name)
+print (user.followers_count)
+
+search = "zerotomastery"
+numberOfTweets = 2
+
+def limit_handle(cursor):
+  while True:
+    try:
+      yield cursor.next()
+    except tweepy.RateLimitError:
+      time.sleep(1000)
+
+
+for follower in limit_handle(tweepy.Cursor(api.followers).items()):
+  if follower.name == 'Usernamehere':
+    print(follower.name)
+    follower.follow()
+
+
+
+for tweet in tweepy.Cursor(api.search, search).items(numberOfTweets):
+    try:
+        tweet.favorite()
+        print('Retweeted the tweet')
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
